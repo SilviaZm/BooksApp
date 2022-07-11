131ceb00febd0a6be5dd9343d86394e4b1c0ad06
SilviaZm <s.zmeu@yahoo.com>
2022-07-11 21:25:07 +0300

    updates

--- functions.js
+++ functions.js
@@ -57,7 +57,7 @@ function populateTable(arr) {
     for (let i = 0; i < arr.length; i++) {
         text += `
         <tr>
-        <td>${arr[i].title}</td>
+        <td class="title">${arr[i].title}</td>
         <td>${arr[i].author}</td>
         <td>${arr[i].genre}</td>
         <td>${arr[i].year}</td>
@@ -74,6 +74,10 @@ function setHome() {
     container.innerHTML = `
     <h1>Books</h1>
     <button class="button__primary btnCreate">Create New Book</button>
+    <div class="search__wrapper">
+        <label>Search by author</label>
+        <input class="searchInput">
+    </div> 
 
    <table>
        <thead>
@@ -93,25 +97,33 @@ function setHome() {
 
 
     let btnAdd = document.querySelector(".btnCreate");
+    let tableBody = document.querySelector('.tableBody');
+    let searchInput = document.querySelector('.searchInput');
+
+    tableBody.addEventListener("click",(e)=>{
+
+        let obj = e.target;
+        if(obj.classList.contains("title")){
+
+            let title=obj.textContent;
+             setUpdateBook(returnBookByTitle(title,booksObj))
+        }
+    })
 
 
     btnAdd.addEventListener("click", () => {
-
-
-
 
         setNewBook();
     })
 
 
+
+
+
 }
 
 
-
 function setNewBook() {
-
-
-
 
     container.innerHTML = `
     <h1>New Book</h1>
@@ -217,33 +229,145 @@ function setNewBook() {
 }
 
 
-function updateBook() {
+function setUpdateBook(book) {
     container.innerHTML = `
     <h1>Update This Book</h1>
     <div class="updateBookWrapper">
         <div>
             <label for="">Title</label>
-            <input type="text" class="titleInputUpdate">
+            <input type="text" class="titleInputUpdate" value="${book.title} " disabled>
         </div>
         <div>
             <label for="">Author</label>
-            <input type="text" class="authorInputUpdate">
+            <input type="text" class="authorInputUpdate" value="${book.author}">
         </div>
         <div>
             <label for="">Genre</label>
-            <input type="text" class="genreInputUpdate">
+            <input type="text" class="genreInputUpdate" value="${book.genre}">
         </div>
         <div>
             <label for="">Year</label>
-            <input type="text" class="yearInputUpdate">
+            <input type="text" class="yearInputUpdate" value="${book.year}">
         </div>
             <label for=""> </label>
             <div>
-                <button class="button__primary updateNewBookCard">Create New Book</button>
+                <button class="button__primary updateButton">Update This Book</button>
                 <button class="button__primary cancelButton">Cancel</button>
+                <button class="button__primary deleteButton">Delete</button>
             </div>
     </div>
-
-    
     `
+
+   
+    let updateButton = document.querySelector('.updateButton');
+    let deleteButton = document.querySelector('.deleteButton');
+    let updateTitleInput = document.querySelector('.titleInputUpdate');
+    let updateAuthorInput = document.querySelector('.authorInputUpdate');
+    let updateGenreInput = document.querySelector('.genreInputUpdate');
+    let updateYearInput = document.querySelector('.yearInputUpdate');
+
+
+
+    updateButton.addEventListener('click', () => {
+
+      
+        
+        let upBook = {
+            title: updateTitleInput.value,
+            author: updateAuthorInput.value,
+            genre: updateGenreInput.value,
+            year: updateYearInput.value
+        }
+        console.log(upBook);
+        updateBook(upBook);
+        console.log(booksObj);
+        setHome();
+    })
+
+    deleteButton.addEventListener('click', () => {
+
+      
+
+        booksObj=booksObj.filter((e)=>!updateTitleInput.value.includes(e.title));
+
+      
+        setHome();
+
+    })
+
+
+}
+
+
+
+//todo functie ce primeste ca parametru aun array si un titlu si returneaza obiectul cu titlul respectiv
+
+function returnBookByTitle(title, arr) {
+
+    for(let i=0; i<arr.length; i++) {
+
+        if(arr[i].title == title) {
+            return arr[i];
+        }
+    }
+
+    return -1;
+}
+
+
+//todo functie ce primeste ca parametru o carte si updateaza in vectorul de carti valorile noi
+
+
+function updateBook(newBook) {
+    
+    for(let i=0; i<booksObj.length; i++) {
+
+        // console.log(booksObj[i].title);
+        console.log(newBook.title);
+        if(newBook.title.includes(booksObj[i].title)) {
+
+            console.log("aici");
+            booksObj[i].author = newBook.author;
+            booksObj[i].genre = newBook.genre;
+            booksObj[i].year = newBook.year;
+            
+        }  
+               
+    }
+
+}
+
+function deleteBook(arr, book) {
+
+    let booksObjDeleted = []
+
+    for(let i=0; i<arr.length; i++) {
+
+        if(book.title.includes(booksObj[i].title) == false ) {
+
+            booksObjDeleted.push(arr[i]);
+
+        }
+    }
+
+
+    console.log(booksObjDeleted);
+
+    return booksObjDeleted;
+}
+
+
+function filterByAuthor(author, arr) {
+
+    let newArr = [];
+
+    for(let i=0; i<arr.length; i++) {
+
+        if(author == arr[i].author) {
+
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
 }
